137b2c48aff0802a9f783084dbabebfb4c520028
ShirukAbdelWahed <shiruk.abdelwahed@edu.fh-joanneum.at>
2019-11-22 10:32:42 +0100

    Klassenname MainActivity auf LoginActivity geaendert

--- app/src/main/java/com/example/bep_bepunctual/LoginActivity.java
+++ app/src/main/java/com/example/bep_bepunctual/LoginActivity.java
@@ -3,7 +3,7 @@ package com.example.bep_bepunctual;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
