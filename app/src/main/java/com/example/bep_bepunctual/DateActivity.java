92896f1d89284df40db11ed4e5a0c3ccab4b62be
HelenaAdam <helena.adam14@gmail.com>
2019-11-22 11:39:23 +0100

    Menu was added

--- app/src/main/java/com/example/bep_bepunctual/DateActivity.java
+++ app/src/main/java/com/example/bep_bepunctual/DateActivity.java
@@ -1,7 +1,10 @@
 package com.example.bep_bepunctual;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DateActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class DateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
+    }
+
+    //Menü
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu__navigator_drawer,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.calendar) {
+            Intent intent = new Intent(this, DateActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.logout) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
